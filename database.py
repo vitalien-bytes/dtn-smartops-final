fecd3b6c758d01b46e2bee6c80a473627986f3d6
vitalien-bytes <contact@digitaltelecomnetwork.fr>
2025-11-13 22:27:39 +0100

    Update database.py

--- database.py
+++ database.py
@@ -4,10 +4,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Utiliser pg8000 à la place de psycopg2
-if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+pg8000://")
+# Correction Aiven -> SQLAlchemy
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+pg8000://")
 
 engine = create_engine(DATABASE_URL, echo=False)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
 Base = declarative_base()
