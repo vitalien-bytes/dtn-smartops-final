55547b2833c16f1eaa45d4b573e335e54c7f26bd
vitalien-bytes <contact@digitaltelecomnetwork.fr>
2025-11-15 07:34:07 +0100

    Update models.py

--- models.py
+++ models.py
@@ -3,19 +3,22 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
 
+
 class Board(Base):
     __tablename__ = "boards"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
 
-class KanbanColumn(Base):   # <-- NOUVEAU NOM
+
+class KanbanColumn(Base):
     __tablename__ = "columns"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
-    board_id = Column(Integer, ForeignKey("boards.id"))
+    board_id = Column(Integer, ForeignKey("boards.id"), nullable=False)
+
 
 class Card(Base):
     __tablename__ = "cards"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
-    column_id = Column(Integer, ForeignKey("columns.id"))
+    column_id = Column(Integer, ForeignKey("columns.id"), nullable=False)
