9326c47be3e2e1eea903d202cb07e1ca00a7f390
vitalien-bytes <contact@digitaltelecomnetwork.fr>
2025-11-16 15:24:33 +0100

    Update database.py

--- database.py
+++ database.py
@@ -4,13 +4,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Aiven fournit postgres://  → convertir en driver pg8000
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+pg8000://")
-
-# IMPORTANT : ne rien ajouter comme connect_args (pg8000 n’accepte pas ssl/sslmode)
 engine = create_engine(
     DATABASE_URL,
+    connect_args={"ssl": True},
     pool_pre_ping=True
 )
 
